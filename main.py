7e8b2026916b632c5fdab9c4bffbe5ee45a93f97
FilipKon <filip.koniu@gmail.com>
2023-02-23 16:55:42 +0100

    new stuff2

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-path = 'C:\\Users\\TARIQOPLATA\\PycharmProjects\\FAERS_final\\data\\Old_gold\\'
+path = 'C:\\Users\\TARIQOPLATA\\PycharmProjects\\FAERS_final\\data\\data\\Old_gold\\'
 
 
 # path = '\Users\'
@@ -9,6 +9,12 @@ path = 'C:\\Users\\TARIQOPLATA\\PycharmProjects\\FAERS_final\\data\\Old_gold\\'
 def main():
     df_f = pd.read_csv(path + 'Disprop_analysis_female_with_HTs.csv')
     df_m = pd.read_csv(path + 'Disprop_analysis_male_with_HTs.csv')
+    hts = 'neurological disorders nec'
+    df_f = df_f[df_f['HT_level2'] == hts]
+    df_m = df_m[df_m['HT_level2'] == hts]
+    aes = df_m['AE'].tolist() + df_f['AE'].tolist()
+    aes = list(dict.fromkeys(aes))
+    print(aes)
 
 
 if __name__ == '__main__':
